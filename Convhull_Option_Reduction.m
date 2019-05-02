2da7f27b3982d76b3eebbd35615cb3cbb32262a3
adrena-lab <47391519+adrena-lab@users.noreply.github.com>
2019-05-02 15:31:37 +0100

    Add files via upload

---
+++ Convhull_Option_Reduction.m
@@ -0,0 +1,182 @@
+function [Illumination_Data_Reduced] = Convhull_Option_Reduction(Illumination_Data, Std_Dev_Selector, Total_Flux_Tolerance, Standard_Deviation_Tolerance)
+
+%This function uses a modified convex hull method to select those points which
+%maximize total flux for a given standard deviation. 
+
+%These lines of code implement the user's selection between the absolute
+%standard deviation and the standard deviation as a percentage of
+%illumination. 
+if Std_Dev_Selector == 1
+    std_dev = Illumination_Data.standard_deviation;
+else
+    std_dev = Illumination_Data.standard_deviation_percentage;    
+end
+
+%Defines the Total_Intensity
+I = Illumination_Data.total_flux;
+
+%Loads additional variables
+data_theta = Illumination_Data.theta;
+data_phi = Illumination_Data.phi;
+data_x_position = Illumination_Data.x_position;
+data_y_position = Illumination_Data.y_position;
+data_height = Illumination_Data.height;
+
+data_Total_Flux = Illumination_Data.total_flux;
+data_Std_Dev = Illumination_Data.standard_deviation;
+data_Std_Perc = Illumination_Data.standard_deviation_percentage;
+
+%Sort the data according to standard deviation from smallest to largest
+[std_dev_sort , In] = sort(std_dev);
+I_sort = I(In);
+
+I = I_sort;
+std_dev = std_dev_sort;
+
+%Perform same sort on other variables
+data_theta = data_theta(In);
+data_phi = data_phi(In);
+data_x_position = data_x_position(In);
+data_y_position = data_y_position(In);
+data_height = data_height(In);
+
+data_Total_Flux = data_Total_Flux(In);
+data_Std_Dev = data_Std_Dev(In);
+data_Std_Perc = data_Std_Perc(In);
+
+%These are the tolerances that the user has already defined
+dx = Standard_Deviation_Tolerance;
+dy = Total_Flux_Tolerance;
+
+%Matlab script takes the standard deviation and total intensity as the x
+%and y values respectively.  It then finds the convex hull of the data set,
+%i.e. a subset of points that enclose all other points in the dataset.  
+%K is a vector of indices for these points
+K = convhull(std_dev, I);
+
+%This convex hull function moves in a counter-clockwise manner, as such, it
+%will first select those points which have a low total flux.  We reverse
+%the order of this vector for ease of later calculations. 
+K = flipud(K);
+
+%Uses the indices to extract the relevant points from the standard
+%deviation and intensity data.  
+Ks = std_dev(K);
+KI = I(K);
+
+%The goal is to achieve a high total intensity with a low standard
+%deviation. Therefore, any points that correspond to a high standard deviation
+%and low total intensity can be disregarded.  As the convhull() function
+%chooses points to encompass the entire range of the data, there will be
+%some points that represent a high standard deviation and low intensity.
+%The following lines of code identify these and removes them from the data
+%set.  
+[~, idx_upper] = max(KI);
+
+Ks = Ks(1:idx_upper);
+KI = KI(1:idx_upper);
+K = K(1:idx_upper);
+
+%The indices for all chosen points will be recorded in this vector 
+Index_all = [];
+
+for i = 1:length(Ks)-1
+    
+    %This creates a subset of data that lies between the ith and ith+1
+    %points selected by the convex hull method
+    std_dev_subset = std_dev(K(i):K(i+1)-1,1);
+    intensity_subset = I(K(i):K(i+1)-1,1);
+    
+    %a linear fit is applied to the two convex hull points 
+    p = polyfit([Ks(i), Ks(i+1)],[KI(i), KI(i+1)],1);
+    
+    %For each standard deviation value from the subset, the corresponding
+    %total flux is calculated based on the linear fit 
+    linear_fit_y = p(1).*std_dev_subset + p(2);
+    
+    %The difference between this calculated value and actual value is
+    %determined
+    difference_y = abs(intensity_subset - linear_fit_y);
+    
+    %If this value is less than the user-defined tolerance, the data point
+    %is saved for further testing.  
+    test_y = difference_y <= dy;
+    
+    %For each total flux value from the subset, the corresponding standard
+    %deviation is calculated based on the linear fit.
+    linear_fit_x = (intensity_subset-p(2))./p(1);
+    
+    %The difference between this value and the actual value is determined 
+    difference_x = abs(std_dev_subset - linear_fit_x);
+    
+    %If this value is less than the user-defined tolerance, the data point
+    %is saved for further testing.  
+    test_x = difference_x <= dx;
+    
+    %If a given data point is within both the total flux tolerance and the
+    %standard deviation tolerance its corresponding value in the test
+    %vector will be 1.  If it lies outside of one or both of these
+    %tolerances it will be 0. 
+    test = test_y.*test_x;
+    
+    %The indices of the acceptable points are extracted
+    index = find(test == 1);
+    
+    %The indices above refer to the data subsets, they are now corrected to
+    %be in line with the overall data set.
+    index = index + K(i) - 1;
+    
+    %The indices are saved
+    Index_all = [Index_all; index];
+    
+end
+
+%This ensures all convex hull points are included in the list of indices of
+%filtered data points
+Index_all = [Index_all; K];
+
+%This removes any duplicates
+Index_unique = unique(Index_all);
+
+%The indices are used to extract the corresponding standard deviation and
+%total flux. 
+X = std_dev(Index_unique);
+Y = I(Index_unique);
+
+%The filtered data is saved as a structure
+Illumination_Data_Reduced = struct;
+
+Illumination_Data_Reduced.theta = data_theta(Index_unique);
+Illumination_Data_Reduced.phi = data_phi(Index_unique);
+Illumination_Data_Reduced.x_position = data_x_position(Index_unique);
+Illumination_Data_Reduced.y_position = data_y_position(Index_unique);
+Illumination_Data_Reduced.height = data_height(Index_unique);
+
+Illumination_Data_Reduced.total_flux = data_Total_Flux(Index_unique);
+Illumination_Data_Reduced.standard_deviation = data_Std_Dev(Index_unique);
+Illumination_Data_Reduced.standard_deviation_percentage = data_Std_Perc(Index_unique);
+
+%This plots the results of the filtering process
+figure('units','normalized','outerposition',[0 0 1 1])
+plot(std_dev, I, '.r', Ks, KI, 'ok', X, Y, '.b', 'MarkerSize', 8)
+
+if Std_Dev_Selector == 1
+    xlabel('Standard Deviation')
+else
+    xlabel('Standard Deviation (% of the Mean Flux)')
+end
+
+ylabel('Total Flux')
+
+title('Convex Hull Option Reduction')
+
+
+dim = [0.625 0.15 .1 .1];
+str = ['Initial Number of Options:  ', num2str(length(Illumination_Data.total_flux))];
+str = [str newline 'Reduced Number of Options:  ', num2str(length(X))];
+t = annotation('textbox',dim,'String',str,'FitBoxToText','on');
+t.FontSize = 14;
+t.BackgroundColor = [1 1 1];
+t.FaceAlpha = 1;
+set(gca,'FontSize',16)
+end
---
+++ Exclude_Unallowed_Combinations.m
@@ -0,0 +1,111 @@
+function [Combinations] = Exclude_Unallowed_Combinations(Combinations) 
+
+%This section of code is used to remove combinations that are unfeasible or
+%not permitted within the physical constraints of the system.  Do NOT use
+%this file without reviewing the constraints and adjusting them for the
+%system in question.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%For a square imaging target, there exists a rectangular pyramid in which
+%the light source cannot be placed or it will obstruct the view of the 
+%imaging device.  
+
+%%%Remove combinations that lie inside the rectangular pyramid%%%
+
+%Vertices of the rectangular base
+%These are the vertices of the domain (rectangle) being imaged
+V1 = [-50, -40, 0];
+V2 = [-50,  40, 0];
+V3 = [ 50,  40, 0];
+V4 = [ 50, -40, 0];
+
+%Vertex of pyramid
+V5 = [0, 0, 110];
+
+Pyramid = [V1; V2; V3; V4; V5];
+
+X_tri = Pyramid(:,1);
+Y_tri = Pyramid(:,2);
+Z_tri = Pyramid(:,3);
+
+%Creates a representation of the rectangular pyramid
+TRI = delaunay(X_tri,Y_tri,Z_tri);
+
+x_positions = Combinations(:,3);
+y_positions = Combinations(:,4);
+H_positions = Combinations(:,5);
+
+%Tests to determine if each x,y,h combination lies within the pyramid
+xyz = [x_positions, y_positions, H_positions];
+tn = tsearchn([X_tri Y_tri Z_tri], TRI, xyz);
+IsInside = ~isnan(tn);
+
+%If the given x,y,h position is outside of the pyramid, the corresponding
+%value IsInside == 0, this vector determines all the combinations that lie
+%outside of the pyramid
+A = find(IsInside == 0);
+
+
+%The possible combinations are reduced to only include those that do not
+%lie within the rectangular pyramid
+Combinations = Combinations(A,:);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%For certain combinations of variable values, the principle axis of the 
+%light source will never intersect with the illuminated area.  These 
+%combinations are eliminated as they can be assumed to be highly
+%inefficient.  Removing them will reduce the necessary computing time.  
+
+%Define the vertices of the illuminated area, may be different from those
+%listed above for the domain being imaged
+V1 = [-50, -40];
+V2 = [-50,  40];
+V3 = [ 50,  40];
+V4 = [ 50, -40];
+
+Area_Illuminated = [V1; V2; V3; V4];
+
+X_tri = Area_Illuminated(:,1);
+Y_tri = Area_Illuminated(:,2);
+
+%Creates a representation of the illuminated area
+TRI = delaunay(X_tri,Y_tri);
+
+%Based on each combination of variables, it is determined where the 
+%principle axis of the light source will intersect with the plane on which 
+%the illuminated area lies.
+theta = Combinations(:,1);
+phi = Combinations(:,2);
+x_position = Combinations(:,3);
+y_position = Combinations(:,4);
+height = Combinations(:,5);
+
+d_along_plate = tan(theta).*height;
+y_along_plate = sin(phi).*d_along_plate;
+x_along_plate = cos(phi).*d_along_plate;
+
+light_centre_x = x_position + x_along_plate;
+light_centre_y = y_position + y_along_plate;
+
+
+%This section tests whether this intersection point lies within the 
+%illuminated area.
+xy = [light_centre_x, light_centre_y];
+tn = tsearchn([X_tri Y_tri], TRI, xy);
+IsInside = ~isnan(tn);
+ 
+%If the given combination of variables produce a beam of light that will
+%strike the desired illuminated area, the value of IsInside == 1.  This
+%vector identifies the index associated with the combinations that satisfy
+%this condition.
+A = find(IsInside == 1);
+ 
+%The possible combinations are reduced to include only those where the
+%centre of the beam of light will strike the desired illuminated area
+Combinations = Combinations(A,:);
+
+
+end
